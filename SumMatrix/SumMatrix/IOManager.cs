35911d19111b4a7baea99c06b95f12fc0ef06cee
Sophie Bouclanoff <yjrbf5250@yandex.ru>
2018-10-28 18:21:33 +0300

    final version

---
+++ SumMatrix/SumMatrix/IOManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumMatrix
+{
+    class IOManager
+    {
+        public double[,] InputMatrix(int width, int height)
+        {
+            double[,] array = new double[width,height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.WriteLine("Элемент № "+(i+1)+", "+(j+1));
+                    array[i, j]=Double.Parse(Console.ReadLine());
+                }
+            }
+
+            return array;
+        }
+
+        public void OutputMatrix(int width, int height, double[,] array)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(array[i, j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- SumMatrix/SumMatrix/Program.cs
+++ SumMatrix/SumMatrix/Program.cs
@@ -10,6 +10,29 @@ namespace SumMatrix
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ширину и высоту матриц");
+            int width, heigth;
+            width = Int32.Parse(Console.ReadLine());
+            heigth = Int32.Parse(Console.ReadLine());
+            IOManager manager = new IOManager();
+            double[,] x = new double[width, heigth];
+            Console.WriteLine("Введите первую матрицу");
+            x = manager.InputMatrix(width,heigth);
+            Console.WriteLine("Первая матрица:");
+            manager.OutputMatrix(width, heigth, x);
+            double[,] y = new double[width, heigth];
+            Console.WriteLine("Введите вторую матрицу");
+            y = manager.InputMatrix(width, heigth);
+            Console.WriteLine("Вторая матрица:");
+            manager.OutputMatrix(width, heigth, y);
+            double[,] result = new double[width, heigth];
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < heigth; j++)
+                    result[i,j]=x[i,j]+y[i,j];
+            Console.WriteLine("Результат:");
+            manager.OutputMatrix(width, heigth, result);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
         }
     }
 }
